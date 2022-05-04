d168cd479b7793b123dbc7c94d08bcbc720ea9f0
karimmoustamid <moustamid.teck@gmail.com>
2022-05-03 21:24:17 -0500

    First Commit

---
+++ MySecoundProject/Greeting/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, Welcome to Greeting! ");
+
+Console.WriteLine("Whats your First Name ?");
+
+var FN = Console.ReadLine();
+
+Console.WriteLine($"Welcome {FN} , to Greeting");
