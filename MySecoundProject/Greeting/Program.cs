11bf46d5f6f75c9f2b41334fa5e126a92c8808ec
karimmoustamid <moustamid.teck@gmail.com>
2022-05-04 19:35:05 -0500

    Second Application

--- MySecoundProject/Greeting/Program.cs
+++ MySecoundProject/Greeting/Program.cs
@@ -1,9 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+Console.WriteLine();
 Console.WriteLine("Hello, Welcome to Greeting! ");
+Console.WriteLine("----------------------------");
+Console.WriteLine();
 
 Console.WriteLine("Whats your First Name ?");
 
 var FN = Console.ReadLine();
 
 Console.WriteLine($"Welcome {FN} , to Greeting");
+
+ 
